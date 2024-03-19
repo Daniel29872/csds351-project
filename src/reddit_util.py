19678aa4c964f2977fa8edb0ffa65faeffa51e6c
Daniel29872 <arnholtdaniel@gmail.com>
2024-03-19 03:51:42 -0400

    add function to get post comment data

--- src/reddit_util.py
+++ src/reddit_util.py
@@ -1,5 +1,31 @@
+import datetime
+
 import praw
+from praw.models import Submission, Comment
 
 reddit = praw.Reddit("bot", user_agent="bot user agent (by u/6uep)")
 
-print(reddit.user.me())
+
+def get_post_data(submission: Submission) -> list[dict]:
+    while True:
+        try:
+            if not submission.comments.replace_more():
+                break
+        except Exception as e:
+            print(e)
+
+    return [
+        {
+            "author": comment.author.name if comment.author else "",
+            "body": comment.body,
+            "created_date": datetime.datetime.fromtimestamp(comment.created_utc),
+            "score": comment.score,
+            "comment_id": comment.id,
+            "submission_id": submission.id,
+            "submission_title": submission.title,
+            "subreddit_id": submission.subreddit.id,
+            "subreddit_title": submission.subreddit.name
+        } 
+        for comment in submission.comments
+    ]
+
