0c2efd21e3a31b44adb54697aa9aad862a4b6ca9
shikohthomas <102746375+shikohthomas@users.noreply.github.com>
2024-03-27 00:59:25 -0400

    Update spacy_util.py

--- src/util/spacy_util.py
+++ src/util/spacy_util.py
@@ -1,7 +1,9 @@
 import spacy
+from aws_util import fetch_posts_from_last_day, update_score
 from spacytextblob.spacytextblob import SpacyTextBlob
 
 nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('spacytextblob')
 
 def get_sentiment_polarity(text):
     doc = nlp(text)
@@ -14,3 +16,6 @@ def process_comments_for_sentiment():
         comment_id, comment_body = comment
         sentiment_polarity = get_sentiment_polarity(comment_body)
         update_score(comment_id, sentiment_polarity)
+
+if __name__ == '__main__':
+    process_comments_for_sentiment()
