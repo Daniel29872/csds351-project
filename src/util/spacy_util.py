33bc2af509e0be6326a02dab727c0787b0561702
shikohthomas <102746375+shikohthomas@users.noreply.github.com>
2024-04-18 20:26:06 -0400

    New Averaged Sentiment with Vader and Textblob
    Added nltk vader sentiment score along side the textblob score

--- src/util/spacy_util.py
+++ src/util/spacy_util.py
@@ -1,20 +1,30 @@
 import spacy
+import nltk
 from aws_util import fetch_posts_from_last_day, update_score
 from spacytextblob.spacytextblob import SpacyTextBlob
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-nlp = spacy.load('model')
+nlp = spacy.load('en_core_web_sm')
+nltk.download('vader_lexicon')
 
-def get_sentiment_polarity(text):
+def get_textblob_sentiment_polarity(text):
+    nlp.add_pipe('spacytextblob')
     doc = nlp(text)
-    sentiment = max(doc.cats, key=doc.cats.get)
-    return doc.cats[sentiment]
+    return doc._.blob.polarity
+
+def get_nltk_sentiment_polarity(text):
+    sia = SentimentIntensityAnalyzer()
+    return sia.polarity_scores(text)['compound']
+
+def get_average_sentiment(text):
+    return (get_textblob_sentiment_polarity(text) + get_nltk_sentiment_polarity(text)) / 2
 
 def process_comments_for_sentiment():
     comments = fetch_posts_from_last_day()
 
     for comment in comments:
         comment_id, comment_body = comment
-        sentiment_polarity = get_sentiment_polarity(comment_body)
+        sentiment_polarity = get_average_sentiment(comment_body)
         update_score(comment_id, sentiment_polarity)
 
 if __name__ == '__main__':
